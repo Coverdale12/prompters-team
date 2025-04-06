a46b9394bf823055d424e53e5f722ba96f93f102
Konstantin Matsa <konstantinmaca@gmail.com>
2025-04-06 15:02:41 +0500

    fix [main]: Fix bad JSON on `/main/startups/`.

--- backend/granter/main/views.py
+++ backend/granter/main/views.py
@@ -8,24 +8,21 @@ def startups_view(request):
     if request.method == 'GET':
         startups = Startup.objects.all()
         try:
-            data = json.loads(request.body)
-            token = data.get('token')
+            # data = json.loads(request.body)
             
-            try:
-                Token.objects.get(token=token)
-
-                return JsonResponse({
-                    'startups': [
-                        dict(filter(
-                            lambda x: not x[0].startswith('_'),
-                            s.__dict__.items(),
-                        )) for s in startups
-                    ]
-                });
-            except Token.DoesNotExist:
-                return JsonResponse({'error': 'Invalid token'}, status=401)
-            except Exception as e:
-                return JsonResponse({'error': str(e)}, status=500)
+            # try:
+            return JsonResponse({
+                'startups': [
+                    dict(filter(
+                        lambda x: not x[0].startswith('_'),
+                        s.__dict__.items(),
+                    )) for s in startups
+                ]
+            });
+            # except Token.DoesNotExist:
+            #     return JsonResponse({'error': 'Invalid token'}, status=401)
+            # except Exception as e:
+            #     return JsonResponse({'error': str(e)}, status=500)
         except json.JSONDecodeError:
             return JsonResponse({'error': 'Invalid JSON'}, status=400)
     else:
